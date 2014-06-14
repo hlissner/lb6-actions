fd89540c1a351b6a2a29d3aa9a26146671e46518
Henrik Lissner <henrik@lissner.net>
2014-06-14 07:32:17 -0400

    Clean up Get External IP Address script

--- Get External IP Address.lbaction/Contents/Scripts/default.js
+++ Get External IP Address.lbaction/Contents/Scripts/default.js
@@ -1,18 +1,16 @@
 function run() {
     var resp = LaunchBar.execute('/usr/bin/curl', '-sf', 'http://checkip.dyndns.org');
-    if (!resp) {
-        LaunchBar.alert("Could not connect to checkip.dyndns.org.");
-        return;
-    }
+    if (resp.trim() !== "") {
+        var result = resp.match(/\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/);
 
-    var result = resp.match(/\b(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\b/);
-    if (result.length > 0) {
+        if (result.length === 0) {
+            LaunchBar.alert("Checkip.dyndns.org responded with deformed output.");
+            return;
+        }
+
         return [{
             title: result[0],
             subtitle: 'External IP Address'
         }];
-    } else {
-        LaunchBar.alert("Checkip.dyndns.org responded with deformed output. Is this script out of date?");
-        return;
     }
 }
