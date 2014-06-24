dbfadfd057e53df3730047777f65713b076bf026
Henrik Lissner <henrik@lissner.net>
2014-06-24 01:41:28 -0400

    Removed favicons from Google Chrome Tabs.lbaction

--- actions/Google Chrome Tabs.lbaction/Contents/Scripts/default.js
+++ actions/Google Chrome Tabs.lbaction/Contents/Scripts/default.js
@@ -15,9 +15,8 @@ function runWithString(string) {
 function runWithItem(item) {
     var data = get_tabs();
     data.forEach(function(tab) {
-        if (tab.url == item.url || tab.url == item) {
-            return switch_tab(tab);
-        }
+        if (tab.url == item.url)
+            switch_tab(tab);
     });
 
     LaunchBar.displayNotification({title: "LaunchBar Error", string: "Can't find the tab."});
@@ -25,44 +24,16 @@ function runWithItem(item) {
 }
 
 ///////////////////////////////
-function tab_select(tab) {
-    if (LaunchBar.options.commandKey) {
-        // Open new tab
-    } else if (LaunchBar.options.shiftKey) {
-        // Close tab
-    }
-}
-
 function switch_tab(tab) {
-    var result = LaunchBar.executeAppleScript(
-        'tell application "Google Chrome"',
-            'set (active tab index of (window '+tab.win_id+')) to '+tab.id,
-            'activate (window '+tab.win_id+')',
-            // 'set miniaturized of (window '+tab.win_id+') to false',
-            'reopen (window '+tab.win_id+')',
-        'end tell'
+    LaunchBar.executeAppleScript(
+        'tell application "Google Chrome" to set (active tab index of (window '+tab.win_id+')) to '+tab.id
     );
 }
 
 function get_tabs() {
-    var result = LaunchBar.executeAppleScript(
-        'tell application "Google Chrome"',
-            'set tabs_result to {}',
-            'set win_id to 1',
-            'repeat with the_window in every window # for every window',
-                'set tab_list to every tab in the_window # get the tabs',
-                'set tab_id to 1',
-                'repeat with the_tab in tab_list # for every tab',
-                    'set tab_url to the URL of the_tab',
-                    'set tab_title to the title of the_tab',
-                    'copy "{\\"id\\": " & tab_id & ", \\"win_id\\": " & win_id & ", \\"title\\":\\"" & tab_title & "\\", \\"url\\":\\"" & tab_url & "\\"}" to end of tabs_result',
-                    'set tab_id to tab_id + 1',
-                'end repeat',
-                'set win_id to win_id + 1',
-            'end repeat',
-            'return tabs_result',
-        'end tell'
-    );
+    var result = LaunchBar.execute("/usr/bin/osascript", "get_chrome_tabs.scpt");
+    if (result === undefined)
+        return false;
 
     var data = JSON.parse('[' + result + ']');
     if (data === undefined)
@@ -70,52 +41,22 @@ function get_tabs() {
 
     var favicon;
     data.forEach(function(tab) {
-        // tab.title = tab.title;
-        if (tab.title !== tab.url) {
+        var hostname = get_hostname(tab.url);
+        if (tab.title !== tab.url)
             tab.subtitle = tab.url;
-        }
 
-        if (tab.title.trim().length === 0) {
+        if (tab.title.length === 0)
             tab.title = get_hostname(tab.url);
-        }
 
-        favicon = get_favicon(tab.url);
-        if (favicon !== false) {
-            tab.icon = favicon.replace(/(\r\n|\n|\r)/,"");
-        }
+        // favicon = get_favicon(tab.url);
+        // if (favicon !== false)
+        //     tab.icon = favicon.substr(0, favicon.length);
 
         tab.action = 'switch_tab';
     });
 
-    LaunchBar.log(JSON.stringify(data));
+    // LaunchBar.log(JSON.stringify(data));
     return data;
-}
-
-function get_favicon(url) {
-    var spos = url.indexOf('//');
-    if (spos === -1 || spos > 19) {
-        return false;
-    }
-
-    var API_URL = "http://www.google.com/s2/favicons?domain=" + url;
-    var hostname = get_hostname(url), 
-        icon = false;
-
-    if (Action.preferences.favicons === undefined) {
-        Action.preferences.favicons = {};
-    } else if (hostname in Action.preferences.favicons) {
-        return Action.preferences.favicons[hostname];
-    }
-
-    var TMP_FILE = '/tmp/'+LaunchBar.bundleIdentifier;
-    LaunchBar.execute('/usr/bin/curl', '-o', TMP_FILE, API_URL);
-    if (File.exists(TMP_FILE)) {
-        var data = LaunchBar.execute('/usr/bin/base64', TMP_FILE);
-        icon = 'data:image/png;base64,' + data.trim();
-        Action.preferences.favicons[hostname] = icon;
-    }
-
-    return icon.replace(/(\r\n|\n|\r)/,"");
 }
 
 function get_hostname(url) {
@@ -128,3 +69,34 @@ function get_hostname(url) {
         return url;
     return url.substr(0, end-(start+2));
 }
+
+// This gets the favicon of the tab and converts it to a Data URI. Google's 
+// service only returns the 16x16 favicons (can't look for a bigger one) -- 
+// which isn't worth the cost in performance.
+//
+// Feel free to enable it if you'd like the functionality though. It still works.
+//
+// function get_favicon(url) {
+//     var spos = url.indexOf('//');
+//     if (spos === -1 || spos > 19) return false;
+//
+//     var hostname = get_hostname(url), 
+//         icon = false;
+//
+//     if (Action.preferences.favicons === undefined) {
+//         Action.preferences.favicons = {};
+//     } else if (hostname in Action.preferences.favicons) {
+//         return Action.preferences.favicons[hostname];
+//     }
+//
+//     var API_URL = "http://www.google.com/s2/favicons?domain=" + url;
+//     var TMP_FILE = '/tmp/'+LaunchBar.bundleIdentifier;
+//     LaunchBar.execute('/usr/bin/curl', '-o', TMP_FILE, API_URL);
+//     if (File.exists(TMP_FILE)) {
+//         var data = LaunchBar.execute('/usr/bin/base64', TMP_FILE);
+//         icon = 'data:image/png;base64,' + data.trim();
+//         Action.preferences.favicons[hostname] = icon;
+//     }
+//
+//     return icon.replace(/(\r\n|\n|\r)/,"");
+// }
