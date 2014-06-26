10c00cb2874fc4fb28e40a3c8f4d7f4e26cf62d3
Henrik Lissner <henrik@lissner.net>
2014-06-26 09:52:29 -0400

    Make Bitly Link: no longer relies on ~/.bitly. Will request API KEY on first use

--- actions/Make Bit.ly URL.lbaction/Contents/Scripts/default.js
+++ actions/Make Bit.ly URL.lbaction/Contents/Scripts/default.js
@@ -1,27 +1,63 @@
 var BITLY_API = "~/.bitly";
 var API_URL = "https://api-ssl.bitly.com";
 
+function run() {
+    getApiKey();
+}
+
 function runWithString(url) {
     var api_key = getApiKey();
-    var resp = HTTP.getJSON(API_URL + "/v3/shorten?access_token="+api_key+"&longUrl="+encodeURIComponent(url)).data;
+    if (!api_key) return;
 
-    if ((!resp || resp.status_code === undefined) || resp.status_code != 200) {
-        LaunchBar.alert("Error "+resp.status_code+": "+resp.status_txt);
-        LaunchBar.alert(JSON.stringify(resp));
-        return;
+    var api_url = API_URL + "/v3/shorten?access_token="+api_key+"&longUrl="+encodeURL(url);
+    var resp = HTTP.getJSON(api_url);
+
+    try {
+        if (resp.error !== undefined)
+            throw "There was a problem querying the API. Response: "+resp.error;
+
+        if (resp.data.status_code !== 200)
+            throw "API error: " + resp.data.status_txt;
+
+        LaunchBar.debugLog("Response: "+JSON.stringify(resp.data));
+
+        return [{
+            title: resp.data.data.url,
+            subtitle: resp.data.data.long_url,
+            url: resp.data.data.long_url
+        }];
+    } catch (err) {
+        LaunchBar.displayNotification({
+            title: "LaunchBar Error", 
+            string: err
+        });
+
     }
-
-    return [
-        {title: resp.data.url, subtitle: resp.data.long_url}
-    ];
 }
 
 // ----------------------
 
 function getApiKey() {
-    if (!File.exists(BITLY_API)) {
-        LaunchBar.alert("Bitly API file is missing. Create it here: ~/.bitly, with your access token in it.");
-        return false;
+    if (Action.preferences.api_key === undefined || LaunchBar.options.alternateKey || LaunchBar.options.shiftKey) {
+        var input = LaunchBar.executeAppleScript('display dialog "Please enter your Bit.ly access token:" default answer ""', 'return text returned of result');
+
+        if (input.trim().length === 0) {
+            notify("You left the API string blank. Try again.");
+            return false;
+        }
+
+        Action.preferences.api_key = input;
     }
-    return File.readText(BITLY_API).trim();
+    return Action.preferences.api_key.replace(/^\s+|\s+$/g, '');
+}
+
+function encodeURL(url) {
+    if (url.indexOf('://') === -1)
+        url = "http://" + url;
+
+    return encodeURIComponent(url);
+}
+
+function notify(msg) {
+    LaunchBar.displayNotification({title: "LaunchBar Error", string: msg});
 }
