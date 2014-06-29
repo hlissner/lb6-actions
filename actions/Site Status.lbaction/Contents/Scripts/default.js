903ba1445af69141e0f37d477095b8c8a0157065
Henrik Lissner <henrik@lissner.net>
2014-06-29 00:35:14 -0400

    Add Site Status.lbaction & screenshots

---
+++ actions/Site Status.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,47 @@
+var API_URL = 'http://www.isup.me/';
+
+function runWithString(string) {
+    try {
+        var resp = HTTP.get(API_URL + string);
+        if (resp.error !== undefined)
+            throw resp.error;
+        if (resp.response.status !== 200)
+            throw resp.response.localizedStatus;
+
+        var html = resp.data;
+
+        var url = get_hostname(string);
+        var m = html.match(/It's (not )?just you[!.]\s*<a href=".+" class="domain">(.+)<\/a>(<\/span>)? ((looks down from here)|(is up))./i);
+        if (m === null) {
+            return [{
+                title: url,
+                subtitle: "Huh? That doesn't look like a site on the interwho.",
+                icon: "404"
+            }];
+        }
+
+        var is_up = m[1] !== "not ";
+        return [{
+            title: url,
+            subtitle: is_up ? "Site is up!" : "Site seems to be down.",
+            url: url,
+            icon: is_up ? "up" : "down"
+        }];
+    } catch (err) {
+        LaunchBar.displayNotification({title: "LaunchBar Error", string: err});
+        LaunchBar.log("ERROR \nURL="+API_URL + string + "\nMSG=" + err);
+    }
+}
+
+function get_hostname(url) {
+    var start = url.indexOf('//');
+    if (start !== -1) 
+        start += 2;
+    else
+        start = 0;
+
+    var end =  url.indexOf('/', start);
+    if (end === -1) return url.substr(start);
+
+    return url.substr(start, end-start);
+}
---
+++ actions/Site Status.lbaction/README.md
@@ -0,0 +1,8 @@
+# Site Status
+
+Checks downforeveryoneorjustme.com to see if a website is up or down (or if it's just
+you).
+
+![](https://raw.githubusercontent.com/hlissner/lb6-actions/master/images/cb-sts-01.png);
+![](https://raw.githubusercontent.com/hlissner/lb6-actions/master/images/cb-sts-02.png);
+![](https://raw.githubusercontent.com/hlissner/lb6-actions/master/images/cb-sts-03.png);
