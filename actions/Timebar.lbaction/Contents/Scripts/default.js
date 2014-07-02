30f2ae0442ef5c1a07b479277d08741528ee0736
Henrik Lissner <henrik@lissner.net>
2014-07-02 13:29:24 -0400

    Timebar: trim input before checking it

--- actions/Timebar.lbaction/Contents/Scripts/default.js
+++ actions/Timebar.lbaction/Contents/Scripts/default.js
@@ -2,7 +2,7 @@ var CLI_PATH = Action.path + "/Contents/Scripts/timebar";
 
 function runWithString(time_str) {
     var resp;
-    if (time_str.toLowerCase() === "stop") {
+    if (time_str.trim().toLowerCase() === "stop") {
         resp = LaunchBar.execute(CLI_PATH, "--stop");
     } else {
         resp = LaunchBar.execute(CLI_PATH, "--parse", time_str);
