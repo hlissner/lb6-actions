686d466c9933d36a835f8033c5f147f8ebdaf029
Henrik Lissner <henrik@lissner.net>
2014-07-05 12:17:17 -0400

    Send to Hastebin -> Hastebin; write code

---
+++ actions/Hastebin.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,63 @@
+include("shared/lib.js");
+
+var API_URL = "http://hastebin.com";
+
+History.MAX_ITEMS = 50;
+
+function run() {
+    try {
+        var history = History.get();
+        if (history.length === 0)
+            return [{title: "No hastebins in history"}];
+
+        return history.map(function(item) {
+            return {
+                title: item[0],
+                url: item[0],
+                subtitle: item[1]
+            };
+        });
+    } catch (err) {
+        Notify.error(err);
+    }
+}
+
+function runWithString(string) {
+    try {
+        var url = API_URL + "/" + post(string);
+        History.add([url, string]);
+        return [{title: url, subtitle: string, url: url}];
+    } catch (err) {
+        Notify.error(err);
+    }
+}
+
+function runWithPaths(paths) {
+    return paths.map(function(path) {
+        try {
+            if (!File.isReadable(path))
+                throw "File isn't readable: "+path;
+
+            var text = File.readText(path);
+            var key = post(text);
+
+            History.add([url, text.substr(0, 30)]);
+            return {
+                title: API_URL + "/" + key,
+                subtitle: Path.basename(path),
+                url: API_URL + "/" + key
+            };
+        } catch (err) {
+            Notify.error(err);
+        }
+    });
+}
+
+function post(string) {
+    var resp = HTTP.postJSON(API_URL+"/documents", {"-d": string});
+    LaunchBar.debugLog(JSON.stringify(resp));
+    if (resp.key === undefined) 
+        throw "Malformed response from hastebin";
+
+    return resp.key;
+}
