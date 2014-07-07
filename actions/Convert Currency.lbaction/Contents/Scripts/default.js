e2c4ee21627f2c79d3a36e3a3f9a32ef2d8a5544
Henrik Lissner <henrik@lissner.net>
2014-07-06 23:21:12 -0400

    Convert Currency (1.1->1.2): shows exchange rate; rounded result

--- actions/Convert Currency.lbaction/Contents/Scripts/default.js
+++ actions/Convert Currency.lbaction/Contents/Scripts/default.js
@@ -12,13 +12,13 @@ function runWithString(string) {
     string = string.trim();
 
     try {
-        var match = string.match(/([\d\.]+)\s*([\w]+)\s*to\s*([\w]+)/);
-        if (match === null || match.length != 4)
+        var match = string.match(/([\d\.]+)\s*([\w]{3})\s*(to|in|:)\s*([\w]{3})/);
+        if (match === null || match.length != 5)
             throw "Your input wasn't formatted correctly!\nProper example: 100 USD to JPY";
 
         var amt = parseFloat(match[1]);
         var from = match[2];
-        var to = match[3];
+        var to = match[4];
 
         var data = request({a: amt, from: from, to: to});
 
@@ -28,12 +28,13 @@ function runWithString(string) {
             throw "Invalid currency code! ...or is Google Finance down?";
 
         // Assemble a readable string
+        var new_amt = parseFloat(result[1]).toFixed(2);
         var res_from = amt + " " + from;
-        var res_to = result[1] + " " + result[2];
+        var res_to = new_amt + " " + result[2];
 
         return [{ 
             title: res_to,
-            subtitle: res_from,
+            subtitle: res_from + " | 1 " + to + " to " + (amt/new_amt).toFixed(3) + " " + from,
             actionArgument: string,
             icon: "money_gold"
         }];
