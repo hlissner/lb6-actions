7f90f19b368e00ccad7eed1880ff959fbb74b3ae
Henrik Lissner <henrik@lissner.net>
2014-11-17 21:53:14 -0500

    Send to Gfycat: allow quicktime *.mov files

--- actions/Send to Gfycat.lbaction/Contents/Scripts/default.js
+++ actions/Send to Gfycat.lbaction/Contents/Scripts/default.js
@@ -20,9 +20,11 @@ function upload(image_path) {
     try
     {
         // Only allow gifs
-        var ext = image_path.split('.').pop();
-        if (ext !== 'gif')
-            throw "Gfycat only accepts gifs";
+        var ext = image_path.split('.').pop().toLowerCase();
+        if (["gif", "mov"].indexOf(ext) === -1)
+            throw "Gfycat only accepts gifs and mov files";
+
+        var mimetype = ext === "gif" ? "image/gif" : "video/quicktime";
 
         // Upload the file
         var key = makeKey();
@@ -35,14 +37,14 @@ function upload(image_path) {
             '-F', 'policy=eyAiZXhwaXJhdGlvbiI6ICIyMDIwLTEyLTAxVDEyOjAwOjAwLjAwMFoiLAogICAgICAgICAgICAiY29uZGl0aW9ucyI6IFsKICAgICAgICAgICAgeyJidWNrZXQiOiAiZ2lmYWZmZSJ9LAogICAgICAgICAgICBbInN0YXJ0cy13aXRoIiwgIiRrZXkiLCAiIl0sCiAgICAgICAgICAgIHsiYWNsIjogInByaXZhdGUifSwKCSAgICB7InN1Y2Nlc3NfYWN0aW9uX3N0YXR1cyI6ICIyMDAifSwKICAgICAgICAgICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLAogICAgICAgICAgICBbImNvbnRlbnQtbGVuZ3RoLXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogICAgICAgICAgICBdCiAgICAgICAgICB9',
             '-F', 'success_action_status=200',
             '-F', 'signature=mk9t/U/wRN4/uU01mXfeTe2Kcoc=',
-            '-F', 'Content-Type=image/gif',
+            '-F', 'Content-Type=' + mimetype,
             '-F', 'file=@'+image_path,
             'https://gifaffe.s3.amazonaws.com/'
         );
         LaunchBar.debugLog("POST: key="+key+"&file=@"+image_path+"\nResponse: "+resp);
         if (resp.indexOf("HTTP/1.1 200 OK") == -1)
             throw "("+resp.error_reason+") "+resp.error_text;
-        
+
         // Tell gfycat to transcode the gif, then return the gfyname
         resp = HTTP.getJSON("http://upload.gfycat.com/transcode/"+key);
         if (resp instanceof Object && resp.data.task === "complete")
