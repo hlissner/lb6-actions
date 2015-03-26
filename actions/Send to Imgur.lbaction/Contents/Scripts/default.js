352ef63aa0b200f3e0958cb2ca280ee8febba58b
Henrik Lissner <henrik@lissner.net>
2015-03-25 23:09:47 -0400

    Add Send to Imgur (for #7)

---
+++ actions/Send to Imgur.lbaction/Contents/Scripts/default.js
@@ -0,0 +1,50 @@
+/*global Lib*/
+
+include("shared/lib.js");
+include("shared/notify.js");
+include("shared/url.js");
+
+var API_URL = "http://api.imgur.com/2/upload";
+var SS_PATH = "/tmp/send2imgur.png";
+
+function run() {
+    LaunchBar.execute("/usr/sbin/screencapture", "-i", "-t", "png", SS_PATH);
+    return runWithPaths([SS_PATH]);
+}
+
+function runWithPaths(paths) {
+    if (Action.preferences.api_key === undefined)
+        Action.preferences.api_key = "26ff5c40cbedf50e7f81124ab473c1cc";
+
+    try {
+        var results = paths.map(upload);
+        if (results.length > 0) {
+            Lib.copy(results.join("\n"));
+            LaunchBar.log(results.join("\n"));
+        }
+    } catch (err) {
+        Lib.Notify.error(err);
+    }
+
+    return results.map(function(url) {
+        return {title: url, url: url};
+    });
+}
+
+function runWithItem(item) {
+    return runWithPaths([item.path]);
+}
+
+//////////////////////////////////////////////
+
+function upload(path) {
+    var response = LaunchBar.execute('/usr/bin/curl',
+                                     '-F', 'key='+Action.preferences.api_key,
+                                     '-F', 'image=@'+path, API_URL);
+    if (response.indexOf("<imgur_page>") === -1) {
+        LaunchBar.log(response);
+        throw "Malformed response";
+    }
+
+    return response.split("<imgur_page>")[1].split("</imgur_page>")[0];
+}
---
+++ actions/Send to Imgur.lbaction/README.md
@@ -0,0 +1,10 @@
+# Send to Imgur
+
+Upload selected image to [Imgur](http://imgur.com); if no image is provided,
+prompt for screenshot.
+
+The link is also copied to the clipboard.
+
+Ported from [aristidesfl](https://github.com/aristidesfl)'s
+[applescript](https://github.com/aristidesfl/launchbar-scripts/blob/master/imgur.applescript).
+
