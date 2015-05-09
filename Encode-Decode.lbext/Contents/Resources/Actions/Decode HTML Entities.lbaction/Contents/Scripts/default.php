d03ea41ab2e549c10fb7cda91e586a7fbf911e3f
Henrik Lissner <henrik@lissner.net>
2015-05-09 17:54:23 -0400

    Encode/Decode HTML: fix bug w/ common html elements not encoding

--- Encode-Decode.lbext/Contents/Resources/Actions/Decode HTML Entities.lbaction/Contents/Scripts/default.php
+++ Encode-Decode.lbext/Contents/Resources/Actions/Decode HTML Entities.lbaction/Contents/Scripts/default.php
@@ -1,9 +1,7 @@
 #!/usr/bin/env php
 <?php
 
-error_reporting(0);
-
-$html = mb_convert_encoding($argv[1], "UTF-8", "HTML-ENTITIES");
+$html = @mb_convert_encoding($argv[1], "UTF-8", "HTML-ENTITIES");
 
 echo json_encode([
     [
--- Encode-Decode.lbext/Contents/Resources/Actions/Encode HTML Entities.lbaction/Contents/Scripts/default.php
+++ Encode-Decode.lbext/Contents/Resources/Actions/Encode HTML Entities.lbaction/Contents/Scripts/default.php
@@ -3,12 +3,14 @@
 
 error_reporting(0);
 
-$html = @mb_convert_encoding($argv[1], 'HTML-ENTITIES', "UTF-8");
+// Handles HTML characters and entities
+$html = @htmlspecialchars($argv[1], ENT_NOQUOTES | ENT_HTML5);
+// Handles unicode/foreign characters
+$html = @mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8");
 
 echo json_encode([
     [
      "title" => $html,
-     "subtitle" => "HTML Entities",
      "actionArgument" => $html,
      "icon" => "html" . (empty($html) ? "_d" : "")
     ]
