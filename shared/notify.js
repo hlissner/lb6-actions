3198cc8e0c3319b3a91cee78db3124ab6d4869da
Henrik Lissner <henrik@lissner.net>
2015-07-18 14:33:17 +0200

    shared/notify.js: more user-friendly errors

--- shared/notify.js
+++ shared/notify.js
@@ -8,11 +8,29 @@
 var Lib = Lib || {};
 
 Lib.Notify = {
-    error: function(msg, data) {
+    error: function(msg, data, header) {
+        header = header || "LaunchBar error";
         msg = msg || "There was an unexpected error.";
+        var args = [header, msg, "Ignore", "Report"];
 
-        LaunchBar.displayNotification({title: "LaunchBar Error", string: msg});
+        if (File.exists(Action.supportPath + "/Preferences.plist"))
+            args.push("Preferences");
+
         if (data)
             LaunchBar.debugLog("ERROR: DATA=" + (data instanceof Object ? JSON.stringify(data) : data));
+
+        switch (LaunchBar.alert.apply(LaunchBar, args)) {
+            case 1:
+                LaunchBar.openURL("https://github.com/hlissner/lb6-actions/issues");
+                break;
+            case 2:
+                LaunchBar.openCommandURL("select?file="+encodeURIComponent(Action.supportPath)+"/Preferences.plist");
+                break;
+        }
+    },
+
+    force_prompt: function(data) {
+        this.error("You forced this prompt to appear by holding down command.",
+                   data, "Forced prompt");
     }
 };
